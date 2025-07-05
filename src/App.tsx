d72fc6c91c0eedfff21babe250568f0526ec14cc
Avi <aviprakashjaiswal@ece.du.ac.in>
2025-07-06 00:51:10 +0530

    chore: Delete unused home route and cleanup app imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<div>404 - Not Found</div>} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-4 text-xl font-bold text-green-600">
-      Welcome to the club! This is the home page.
-    </div>
-  )
-}
